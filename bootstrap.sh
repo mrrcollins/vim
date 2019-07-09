5c3a644d1cc9933c7b2564feb03449707068844c
Ryan Collins <gozar@Ryans-MacBook-Pro-4.local>
2019-07-09 16:31:25 -0400

    Keeping plugins in sync is a pain. This will work for now.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,36 +2,39 @@
 
 if [ ! -d ~/.vim/bundle ]; then
 	mkdir ~/.vim/bundle
-	cd ~/.vim/bundle
-
-    echo "Cloning tlib..."
-	git clone --quiet https://github.com/tomtom/tlib_vim.git
-    echo "Cloning vim-addon-mw-utils..."
-	git clone --quiet https://github.com/MarcWeber/vim-addon-mw-utils.git
-    echo "Cloning Ultisnippets..."
-	git clone --quiet https://github.com/SirVer/ultisnips.git
-    echo "Cloning vim-snippets..."
-	git clone --quiet https://github.com/honza/vim-snippets.git
-    echo "Cloning vim-puppet..."
-	git clone --quiet https://github.com/rodjek/vim-puppet.git
-    echo "Cloning nerdtree..."
-	git clone --quiet https://github.com/scrooloose/nerdtree.git
-    echo "Cloning vim-mads..."
-	git clone --quiet https://github.com/skrzyp/vim-mads
-    echo "Cloning vim-surround..."
-	git clone --quiet https://github.com/tpope/vim-surround.git
-    echo "Cloning vim-pencil..."
-	git clone --quiet https://github.com/reedes/vim-pencil
-    echo "Cloning vim-autoread..."
-	git clone --quiet https://github.com/djoshea/vim-autoread.git
-    echo "Cloning Goyo..."
-	git clone --quiet https://github.com/junegunn/goyo.vim.git
-    echo "Cloning vim-markdown..."
-	git clone --quiet https://github.com/plasticboy/vim-markdown.git
-    echo "Cloning vim-move..."
-    git clone --quiet https://github.com/matze/vim-move.git
 else
 	echo "~/.vim/bundle folder already exists..."
 fi
 
+cd ~/.vim/bundle
+
+echo "Attempting to clone tlib..."
+[ ! -d tlib_vim ] && git clone --quiet https://github.com/tomtom/tlib_vim.git
+echo "Attempting to clone vim-addon-mw-utils..."
+[ ! -d vim-addon-mw-utils ] && git clone --quiet https://github.com/MarcWeber/vim-addon-mw-utils.git
+echo "Attempting to clone Ultisnippets..."
+[ ! -d ultisnips ] && git clone --quiet https://github.com/SirVer/ultisnips.git
+echo "Attempting to clone vim-snippets..."
+[ ! -d vim-snippets ] && git clone --quiet https://github.com/honza/vim-snippets.git
+echo "Attempting to clone vim-puppet..."
+[ ! -d vim-puppet ] && git clone --quiet https://github.com/rodjek/vim-puppet.git
+echo "Attempting to clone nerdtree..."
+[ ! -d nerdtree ] && git clone --quiet https://github.com/scrooloose/nerdtree.git
+echo "Attempting to clone vim-mads..."
+[ ! -d vim-mads ] && git clone --quiet https://github.com/skrzyp/vim-mads
+echo "Attempting to clone vim-surround..."
+[ ! -d vim-surround ] && git clone --quiet https://github.com/tpope/vim-surround.git
+echo "Attempting to clone vim-pencil..."
+[ ! -d vim-pencil ] && git clone --quiet https://github.com/reedes/vim-pencil
+echo "Attempting to clone vim-autoread..."
+[ ! -d vim-autoread ] && git clone --quiet https://github.com/djoshea/vim-autoread.git
+echo "Attempting to clone Goyo..."
+[ ! -d goyo.vim ] && git clone --quiet https://github.com/junegunn/goyo.vim.git
+echo "Attempting to clone vim-markdown..."
+[ ! -d vim-markdown ] && git clone --quiet https://github.com/plasticboy/vim-markdown.git
+echo "Attempting to clone vim-move..."
+[ ! -d vim-move ] && git clone --quiet https://github.com/matze/vim-move.git
+
 [ ! -L ~/.vimrc ] && ln -s ~/.vim/.vimrc ~/.vimrc || echo "Symlink already exists..."
+
+cd ~/.vim
